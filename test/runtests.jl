ccc34843a9bfc5a7f3b923d66cbfb8d3e1fdc856
Kuts Michael <kutsjuice@gmail.com>
2023-09-28 00:26:06 +0300

    norms testset added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using MioisHomework
 using Test
+import LinearAlgebra as LA
 
 @testset "getdigits" begin
     @test getdigits(1234) == [1, 2, 3, 4];
@@ -11,4 +12,14 @@ end
     @test touppercase("julia") == "JULIA";
     @test toappercase("hello World") == "HELLO WORLD";
     @test toappercase("dsfdsf43243HH,,.sdfd&*(&(*HGJSdsghfdsjkh***))") == uppercase("dsfdsf43243HH,,.sdfd&*(&(*HGJSdsghfdsjkh***))");
+end
+
+@testset "norms" begin
+    a = rand(10);
+    @test firstnorm(a) == LA.norm(a,1);
+    @test secondnorm(a) == LA.norm(a, 2);
+    @test infnorm(a) == LA.norm(a, Inf);
+    b = rand(10, 10);
+    @test firstnorm(b) == LA.opnorm(b, 1);
+    @test infnorm(b) == LA.opnorm(b, Inf);
 end
