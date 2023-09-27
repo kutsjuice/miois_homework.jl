21eebc73b3bb4bdc0885424bb4a825002f0e2ae2
Kuts Michael <kutsjuice@gmail.com>
2023-09-28 00:08:29 +0300

    vector norms templates added

--- src/MioisHomework.jl
+++ src/MioisHomework.jl
@@ -10,6 +10,17 @@ function touppercase(str_)
     return 0;
 end
 
+function firstnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
+
+function secondnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
+
+function infnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
 
 
 end
