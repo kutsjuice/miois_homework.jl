85b0415a256885e4c9a425e09c07e3bb39013b81
Kuts Michael <kutsjuice@gmail.com>
2023-09-27 23:43:51 +0300

    get digits implemented

--- src/MioisHomework.jl
+++ src/MioisHomework.jl
@@ -3,7 +3,14 @@ module MioisHomework
 export getdigits
 
 function getdigits(a)
-    return 0
+    ret = [];
+    b = a;
+    while (b/10) > 1
+        push!(ret, b%10);
+        b = b÷10;
+    end
+    push!(ret, b);
+    return ret[end:-1:1];
 end
 
 end
