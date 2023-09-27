0fc5eaa79d65edc883758c29cba33cff44acd3bf
Kuts Michael <kutsjuice@gmail.com>
2023-09-28 00:19:56 +0300

    touppercase testset added

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,3 +6,9 @@ using Test
     @test getdigits(0123) == [1, 2, 3];
     @test getdigits(999999999999) == [9,9,9,9,9,9,9,9,9,9,9,9];
 end
+
+@testset "touppercase" begin
+    @test touppercase("julia") == "JULIA";
+    @test toappercase("hello World") == "HELLO WORLD";
+    @test toappercase("dsfdsf43243HH,,.sdfd&*(&(*HGJSdsghfdsjkh***))") == uppercase("dsfdsf43243HH,,.sdfd&*(&(*HGJSdsghfdsjkh***))");
+end
