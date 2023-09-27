36808398f00f4574ce077c322b24509508c5c9b3
Kuts Michael <kutsjuice@gmail.com>
2023-09-28 00:16:24 +0300

    task #1 shifted to separate file

--- src/MioisHomework.jl
+++ src/MioisHomework.jl
@@ -1,41 +1,5 @@
 module MioisHomework
 
-export getdigits
-
-function getdigits(a)
-    return 0
-end
-
-function touppercase(str_)
-    return 0;
-end
-
-function firstnorm(vec_::AbstractVector{<:Number})
-    return 0;
-end
-
-function secondnorm(vec_::AbstractVector{<:Number})
-    return 0;
-end
-
-function infnorm(vec_::AbstractVector{<:Number})
-    return 0;
-end
-
-function firstnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
-end
-
-function infnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
-end
-
-function isleap(year)
-    return "leap";
-end
-
-function chesscolor(cell1, cell2)
-    return false;
-end
+include("task01.jl")
 
 end
---
+++ src/task01.jl
@@ -0,0 +1,37 @@
+export getdigits, touppercase, firstnorm, secondnorm, infnorm, isleap, chesscolor
+
+function getdigits(a)
+    return 0
+end
+
+function touppercase(str_)
+    return 0;
+end
+
+function firstnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
+
+function secondnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
+
+function infnorm(vec_::AbstractVector{<:Number})
+    return 0;
+end
+
+function firstnorm(vec_::AbstractMatrix{<:Number})
+    return 0;
+end
+
+function infnorm(vec_::AbstractMatrix{<:Number})
+    return 0;
+end
+
+function isleap(year)
+    return "leap";
+end
+
+function chesscolor(cell1, cell2)
+    return false;
+end
