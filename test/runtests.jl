1caa48cdc9d1168e1f691d94cf6a936d84836c76
Kuts Michael <kutsjuice@gmail.com>
2023-09-27 23:47:58 +0300

    getdigits tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using MioisHomework
 using Test
 
 @testset "MioisHomework.jl" begin
-
+    @test getdigits(1234) == [1, 2, 3, 4];
+    @test getdigits(0123) == [1, 2, 3];
+    @test getdigits(999999999999) == [9,9,9,9,9,9,9,9,9,9,9,9];
 end
