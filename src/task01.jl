d4bfe86ba204883d1c17f02eb0d28de9dba0b544
Kuts Michael <kutsjuice@gmail.com>
2023-09-28 01:26:49 +0300

    solved implemented

--- src/task01.jl
+++ src/task01.jl
@@ -1,37 +1,98 @@
 export getdigits, touppercase, firstnorm, secondnorm, infnorm, isleap, chesscolor
 
 function getdigits(a)
-    return 0
+    ret = [];
+    b = a;
+    while (b/10) > 1
+        push!(ret, b%10);
+        b = b÷10;
+    end
+    push!(ret, b);
+    return ret[end:-1:1];
 end
 
 function touppercase(str_)
-    return 0;
+    buf = [];
+    for sym in str_
+        if (sym>='a')&&(sym<='z')
+            push!(buf, sym + ('A' - 'a'));
+        else
+            push!(buf, sym);
+        end
+    end
+    return join(buf);
 end
 
 function firstnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    ret = 0;
+    for num in vec_
+        ret += abs(num);
+    end
+    return ret;
 end
 
 function secondnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    ret = 0;
+    for num in vec_
+        ret += num^2;
+    end
+    return sqrt(ret);
 end
 
 function infnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    ret = 0;
+    for num in vec_
+        if num > ret
+            ret = num;
+        end
+    end
+    return ret;
 end
 
-function firstnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
+function firstnorm(mat_::AbstractMatrix{<:Number})
+    ret = 0;
+    for col in eachcol(mat_)
+        buf = 0
+        for el in col
+            buf += el;
+        end
+        ret = ret < buf ? buf : ret;
+    end
+    return ret;
 end
 
-function infnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
+function infnorm(mat_::AbstractMatrix{<:Number})
+    ret = 0;
+    for col in eachrow(mat_)
+        buf = 0
+        for el in col
+            buf+=el;
+        end
+        ret = ret < buf ? buf : ret;
+    end
+    return ret;
 end
 
 function isleap(year)
+    if (year % 400) == 0
+        return true;
+    elseif (year % 100) == 0
+        return false
+    elseif (year % 4) == 0
+        return true
+    end
     return false;
 end
 
 function chesscolor(cell1, cell2)
-    return false;
+    a = cell1[1] - 'a' + 1;
+    b = cell1[2];
+
+    c = cell2[1] - 'a' + 1;
+    d = cell2[2];
+
+    if ((a+b+c+d) % 2) == 0
+        return true;
+    end
+    return false; 
 end
