803e8dd9d08b6fd6a26c9afa15d0451d2c8137d5
Spiwini <76060287+Spiwini@users.noreply.github.com>
2026-01-20 23:57:40 +0300

    Update task02.jl

--- src/task02.jl
+++ src/task02.jl
@@ -1,10 +1,17 @@
 function arraysum(ar::AbstractVector{<:Number})::Float64
     sum = 0;
+    for x in ar
+        sum+= x
+    end
     return sum;
 end
 
-
 function mul35sum(N::Integer)::Int64
-    sum = Int64(0);
-    return sum;
+    sum = 0
+    for i in 1:(N-1) 
+        if i % 3 == 0 || i % 5 == 0
+            sum += i
+        end
+    end
+    return total
 end
